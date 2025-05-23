bf18cd5187c5e7454e845a62a222859865dded99
OrlandoJim24 <orlandojim2023@gmail.com>
2025-05-23 20:57:36 +0000

    Finished Project

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [Red, setRed] = useState("btn-danger opacity-25");
+	const [Yellow, setYellow] = useState("btn-warning opacity-25");
+	const [Green, setGreen] = useState("btn-success opacity-25");
+	
+
+	const redLight = () => {
+		if (Red === "btn-danger opacity-25") {
+			setRed("btn-danger");
+		} else {
+			setRed("btn-danger opacity-25");
+		}
+	};
+
+	const yellowLight = () => {
+		if (Yellow === "btn-warning opacity-25") {
+			setYellow("btn-warning");
+		} else {
+			setYellow("btn-warning opacity-25");
+		}
+	};
+
+	const greenLight = () => {
+		if (Green === "btn-success opacity-25") {
+			setGreen("btn-success");
+		} else {
+			setGreen("btn-success opacity-25");
+		}
+	};
+
+	
+	
+	return (
+		<div className="d-flex justify-content-evenly">
+			<div className="d-flex flex-column align-items-center justify-content-center mt-5">
+				<div className="bg-dark" style= {{ width: "40px", height: "120px" }}></div>
+				<div className="bg-dark" style={{ width: "150px", height: "auto", paddingBottom: "50px", borderRadius: '30px' }}>
+					<div className="d-flex flex-column" aria-label="Basic mixed styles example">
+						<button type="button" className={`btn ${Red} mt-5 mx-4 rounded-circle`} style={{ height: "90px" }} onClick={redLight}>
+						</button>
+						<button type="button" className={`btn ${Yellow} mt-5 mx-4 rounded-circle`} style={{ height: "90px" }} onClick={yellowLight}>
+						</button>
+						<button type="button" className={`btn ${Green} mt-5 mx-4 rounded-circle`} style={{ height: "90px" }} onClick={greenLight}>
+						</button>
+                    
+					</div>
+				</div>
+			</div>
+			
 		</div>
 	);
 };
